ethereum/evmone

Refactor execution unit testing

test/unittests/test_execution.cpp
@@ -8,36 +8,51 @@
 
 #include <gtest/gtest.h>
 
-TEST(execution, push_and_pop)
+class execution : public testing::Test
 {
-    auto code = from_hex("610102506801020304050607080950");
+protected:
+    evmc_result result = {};
 
-    auto r = evmone::execute(11, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 1);
+    ~execution() noexcept override
+    {
+        // Release the attached EVM execution result.
+        if (result.release)
+            result.release(&result);
+    }
+
+    /// Wrapper for evmone::execute. The result will be in the .result field.
+    void execute(int64_t gas, std::string_view code_hex) noexcept
+    {
+        // TODO: Use string_view in from_hex()?
+        auto code = from_hex(code_hex.data());
+        result = evmone::execute(gas, &code[0], code.size());
+    }
+};
+
+TEST_F(execution, push_and_pop)
+{
+    execute(11, "610102506801020304050607080950");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 1);
 }
 
-TEST(execution, stack_underflow)
+TEST_F(execution, stack_underflow)
 {
-    auto code = from_hex("61010250680102030405060708095050");
-
-    auto r = evmone::execute(13, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_STACK_UNDERFLOW);
-    EXPECT_EQ(r.gas_left, 0);
+    execute(13, "61010250680102030405060708095050");
+    EXPECT_EQ(result.status_code, EVMC_STACK_UNDERFLOW);
+    EXPECT_EQ(result.gas_left, 0);
 }
 
-TEST(execution, add)
+TEST_F(execution, add)
 {
-    auto code = from_hex("6007600d0160005260206000f3");
-    auto r = evmone::execute(25, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 1);
-    EXPECT_EQ(r.output_size, 32);
-    EXPECT_EQ(r.output_data[31], 20);
-    r.release(&r);
+    execute(25, "6007600d0160005260206000f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 1);
+    EXPECT_EQ(result.output_size, 32);
+    EXPECT_EQ(result.output_data[31], 20);
 }
 
-TEST(execution, dup)
+TEST_F(execution, dup)
 {
     // 0 7 3 5
     // 0 7 3 5 3 5
@@ -45,61 +60,51 @@ TEST(execution, dup)
     // 0 7 3 5 20
     // 0 7 3 5 (20 0)
     // 0 7 3 5 3 0
-    auto code = from_hex("6000600760036005818180850101018452602084f3");
-    auto r = evmone::execute(49, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 1);
-    EXPECT_EQ(r.output_size, 32);
-    EXPECT_EQ(r.output_data[31], 20);
-    r.release(&r);
+    execute(49, "6000600760036005818180850101018452602084f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 1);
+    EXPECT_EQ(result.output_size, 32);
+    EXPECT_EQ(result.output_data[31], 20);
 }
 
-TEST(execution, sub_and_swap)
+TEST_F(execution, sub_and_swap)
 {
-    auto code = from_hex("600180810380829052602090f3");
-    auto r = evmone::execute(33, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    EXPECT_EQ(r.output_size, 32);
-    EXPECT_EQ(r.output_data[31], 1);
-    r.release(&r);
+    execute(33, "600180810380829052602090f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    EXPECT_EQ(result.output_size, 32);
+    EXPECT_EQ(result.output_data[31], 1);
 }
 
-TEST(execution, memory_and_not)
+TEST_F(execution, memory_and_not)
 {
-    auto code = from_hex("600060018019815381518252800190f3");
-    auto r = evmone::execute(42, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    EXPECT_EQ(r.output_size, 2);
-    EXPECT_EQ(r.output_data[1], 0xfe);
-    EXPECT_EQ(r.output_data[0], 0);
-    r.release(&r);
+    execute(42, "600060018019815381518252800190f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    EXPECT_EQ(result.output_size, 2);
+    EXPECT_EQ(result.output_data[1], 0xfe);
+    EXPECT_EQ(result.output_data[0], 0);
 }
 
-TEST(execution, msize)
+TEST_F(execution, msize)
 {
-    auto code = from_hex("60aa6022535960005360016000f3");
-    auto r = evmone::execute(29, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    EXPECT_EQ(r.output_size, 1);
-    EXPECT_EQ(r.output_data[0], 0x23);
-    r.release(&r);
+    execute(29, "60aa6022535960005360016000f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    EXPECT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 0x23);
 }
 
-TEST(execution, gas)
+TEST_F(execution, gas)
 {
-    auto code = from_hex("5a5a5a010160005360016000f3");
-    auto r = evmone::execute(40, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 13);
-    EXPECT_EQ(r.output_size, 1);
-    EXPECT_EQ(r.output_data[0], 38 + 36 + 34);
-    r.release(&r);
+    execute(40, "5a5a5a010160005360016000f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 13);
+    EXPECT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 38 + 36 + 34);
 }
 
-TEST(execution, arith)
+TEST_F(execution, arith)
 {
     // x = (0 - 1) * 3
     // y = 17 s/ x
@@ -113,16 +118,14 @@ TEST(execution, arith)
     s += "910201";                // 17 17
     s += "0315";                  // 1
     s += "60005360016000f3";
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(100, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 26);
-    EXPECT_EQ(r.output_size, 1);
-    EXPECT_EQ(r.output_data[0], 1);
-    r.release(&r);
+    execute(100, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 26);
+    EXPECT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 1);
 }
 
-TEST(execution, comparison)
+TEST_F(execution, comparison)
 {
     std::string s;
     s += "60006001808203";  // 0 1 -1
@@ -132,20 +135,18 @@ TEST(execution, comparison)
     s += "818113600353";    // m[3] = -1 s> 1
     s += "818114600453";    // m[4] = -1 == 1
     s += "60056000f3";
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(100, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 1);
-    EXPECT_EQ(r.output_size, 5);
-    EXPECT_EQ(r.output_data[0], 0);
-    EXPECT_EQ(r.output_data[1], 1);
-    EXPECT_EQ(r.output_data[2], 1);
-    EXPECT_EQ(r.output_data[3], 0);
-    EXPECT_EQ(r.output_data[4], 0);
-    r.release(&r);
+    execute(100, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 1);
+    EXPECT_EQ(result.output_size, 5);
+    EXPECT_EQ(result.output_data[0], 0);
+    EXPECT_EQ(result.output_data[1], 1);
+    EXPECT_EQ(result.output_data[2], 1);
+    EXPECT_EQ(result.output_data[3], 0);
+    EXPECT_EQ(result.output_data[4], 0);
 }
 
-TEST(execution, bitwise)
+TEST_F(execution, bitwise)
 {
     std::string s;
     s += "60aa60ff";      // aa ff
@@ -153,18 +154,16 @@ TEST(execution, bitwise)
     s += "818117600153";  // m[1] = aa | ff
     s += "818118600253";  // m[2] = aa ^ ff
     s += "60036000f3";
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(60, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 3);
-    EXPECT_EQ(r.output_data[0], 0xaa & 0xff);
-    EXPECT_EQ(r.output_data[1], 0xaa | 0xff);
-    EXPECT_EQ(r.output_data[2], 0xaa ^ 0xff);
-    r.release(&r);
+    execute(60, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 3);
+    EXPECT_EQ(result.output_data[0], 0xaa & 0xff);
+    EXPECT_EQ(result.output_data[1], 0xaa | 0xff);
+    EXPECT_EQ(result.output_data[2], 0xaa ^ 0xff);
 }
 
-TEST(execution, jump)
+TEST_F(execution, jump)
 {
     std::string s;
     s += "60be600053";  // m[0] = be
@@ -175,33 +174,29 @@ TEST(execution, jump)
     s += "5b";          // JUMPDEST
     s += "600153";      // m[1] = fa
     s += "60026000f3";  // RETURN(0,2)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(44, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 2);
-    EXPECT_EQ(r.output_data[0], 0xbe);
-    EXPECT_EQ(r.output_data[1], 0xfa);
-    r.release(&r);
+    execute(44, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 2);
+    EXPECT_EQ(result.output_data[0], 0xbe);
+    EXPECT_EQ(result.output_data[1], 0xfa);
 }
 
-TEST(execution, jumpi)
+TEST_F(execution, jumpi)
 {
     std::string s;
     s += "5a600557";      // GAS 5 JUMPI
     s += "00";            // STOP
     s += "5b60016000f3";  // JUMPDEST RETURN(0,1)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(25, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 1);
-    EXPECT_EQ(r.output_data[0], 0);
-    r.release(&r);
+    execute(25, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 0);
 }
 
 
-TEST(execution, byte)
+TEST_F(execution, byte)
 {
     std::string s;
     s += "63aabbccdd";  // aabbccdd
@@ -212,18 +207,16 @@ TEST(execution, byte)
     s += "80601f1a";    // DUP 31 BYTE
     s += "600453";      // m[4] = dd
     s += "60056000f3";  // RETURN(0,5)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(57, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 5);
-    EXPECT_EQ(r.output_data[0], 0);
-    EXPECT_EQ(r.output_data[2], 0xaa);
-    EXPECT_EQ(r.output_data[4], 0xdd);
-    r.release(&r);
+    execute(57, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 5);
+    EXPECT_EQ(result.output_data[0], 0);
+    EXPECT_EQ(result.output_data[2], 0xaa);
+    EXPECT_EQ(result.output_data[4], 0xdd);
 }
 
-TEST(execution, addmod_mulmod)
+TEST_F(execution, addmod_mulmod)
 {
     std::string s;
     s += "7fcdeb8272fc01d4d50a6ec165d2ea477af19b9b2c198459f59079583b97e88a66";
@@ -234,19 +227,17 @@ TEST(execution, addmod_mulmod)
     s += "82828209";    // DUP DUP DUP MULMOD
     s += "602052";      // m[32..]
     s += "60406000f3";  // RETURN(0,64)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(67, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 64);
+    execute(67, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 64);
     auto a = from_hex("65ef55f81fe142622955e990252cb5209a11d4db113d842408fd9c7ae2a29a5a");
-    EXPECT_EQ(a, bytes(&r.output_data[0], 32));
+    EXPECT_EQ(a, bytes(&result.output_data[0], 32));
     auto p = from_hex("34e04890131a297202753cae4c72efd508962c9129aed8b08c8e87ab425b7258");
-    EXPECT_EQ(p, bytes(&r.output_data[32], 32));
-    r.release(&r);
+    EXPECT_EQ(p, bytes(&result.output_data[32], 32));
 }
 
-TEST(execution, signextend)
+TEST_F(execution, signextend)
 {
     std::string s;
     s += "62017ffe";    // 017ffe
@@ -255,19 +246,17 @@ TEST(execution, signextend)
     s += "8060010b";    // DUP SIGNEXTEND(1)
     s += "602052";      // m[32..]
     s += "60406000f3";  // RETURN(0,64)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(49, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 64);
+    execute(49, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 64);
     auto a = from_hex("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe");
-    EXPECT_EQ(bytes(&r.output_data[0], 32), a);
+    EXPECT_EQ(bytes(&result.output_data[0], 32), a);
     auto b = from_hex("0000000000000000000000000000000000000000000000000000000000007ffe");
-    EXPECT_EQ(bytes(&r.output_data[32], 32), b);
-    r.release(&r);
+    EXPECT_EQ(bytes(&result.output_data[32], 32), b);
 }
 
-TEST(execution, exp)
+TEST_F(execution, exp)
 {
     std::string s;
     s += "612019";      // 0x2019
@@ -275,12 +264,10 @@ TEST(execution, exp)
     s += "0a";          // EXP
     s += "600052";      // m[0..]
     s += "60206000f3";  // RETURN(0,32)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(131, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 32);
+    execute(131, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 32);
     auto a = from_hex("263cf24662b24c371a647c1340022619306e431bf3a4298d4b5998a3f1c1aaa3");
-    EXPECT_EQ(bytes(&r.output_data[0], 32), a);
-    r.release(&r);
+    EXPECT_EQ(bytes(&result.output_data[0], 32), a);
 }

luxcpp/cevm

test/unittests/test_execution.cpp
@@ -8,36 +8,56 @@
 
 #include <gtest/gtest.h>
 
-TEST(execution, push_and_pop)
+class execution : public testing::Test
 {
-    auto code = from_hex("610102506801020304050607080950");
+protected:
+    evmc_result result = {};
 
-    auto r = evmone::execute(11, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 1);
+    ~execution() noexcept override
+    {
+        // Release the attached EVM execution result.
+        if (result.release)
+            result.release(&result);
+    }
+
+    /// Wrapper for evmone::execute. The result will be in the .result field.
+    void execute(int64_t gas, std::string_view code_hex, std::string_view input_hex = {}) noexcept
+    {
+        // TODO: Use string_view in from_hex()?
+        auto code = from_hex(code_hex.data());
+        auto input = from_hex(input_hex.data());
+        auto msg = evmc_message{};
+        msg.gas = gas;
+        msg.input_data = input.data();
+        msg.input_size = input.size();
+        result = evmone::execute(&msg, &code[0], code.size());
+    }
+};
+
+TEST_F(execution, push_and_pop)
+{
+    execute(11, "610102506801020304050607080950");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 1);
 }
 
-TEST(execution, stack_underflow)
+TEST_F(execution, stack_underflow)
 {
-    auto code = from_hex("61010250680102030405060708095050");
-
-    auto r = evmone::execute(13, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_STACK_UNDERFLOW);
-    EXPECT_EQ(r.gas_left, 0);
+    execute(13, "61010250680102030405060708095050");
+    EXPECT_EQ(result.status_code, EVMC_STACK_UNDERFLOW);
+    EXPECT_EQ(result.gas_left, 0);
 }
 
-TEST(execution, add)
+TEST_F(execution, add)
 {
-    auto code = from_hex("6007600d0160005260206000f3");
-    auto r = evmone::execute(25, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 1);
-    EXPECT_EQ(r.output_size, 32);
-    EXPECT_EQ(r.output_data[31], 20);
-    r.release(&r);
+    execute(25, "6007600d0160005260206000f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 1);
+    EXPECT_EQ(result.output_size, 32);
+    EXPECT_EQ(result.output_data[31], 20);
 }
 
-TEST(execution, dup)
+TEST_F(execution, dup)
 {
     // 0 7 3 5
     // 0 7 3 5 3 5
@@ -45,61 +65,51 @@ TEST(execution, dup)
     // 0 7 3 5 20
     // 0 7 3 5 (20 0)
     // 0 7 3 5 3 0
-    auto code = from_hex("6000600760036005818180850101018452602084f3");
-    auto r = evmone::execute(49, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 1);
-    EXPECT_EQ(r.output_size, 32);
-    EXPECT_EQ(r.output_data[31], 20);
-    r.release(&r);
+    execute(49, "6000600760036005818180850101018452602084f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 1);
+    EXPECT_EQ(result.output_size, 32);
+    EXPECT_EQ(result.output_data[31], 20);
 }
 
-TEST(execution, sub_and_swap)
+TEST_F(execution, sub_and_swap)
 {
-    auto code = from_hex("600180810380829052602090f3");
-    auto r = evmone::execute(33, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    EXPECT_EQ(r.output_size, 32);
-    EXPECT_EQ(r.output_data[31], 1);
-    r.release(&r);
+    execute(33, "600180810380829052602090f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    EXPECT_EQ(result.output_size, 32);
+    EXPECT_EQ(result.output_data[31], 1);
 }
 
-TEST(execution, memory_and_not)
+TEST_F(execution, memory_and_not)
 {
-    auto code = from_hex("600060018019815381518252800190f3");
-    auto r = evmone::execute(42, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    EXPECT_EQ(r.output_size, 2);
-    EXPECT_EQ(r.output_data[1], 0xfe);
-    EXPECT_EQ(r.output_data[0], 0);
-    r.release(&r);
+    execute(42, "600060018019815381518252800190f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    EXPECT_EQ(result.output_size, 2);
+    EXPECT_EQ(result.output_data[1], 0xfe);
+    EXPECT_EQ(result.output_data[0], 0);
 }
 
-TEST(execution, msize)
+TEST_F(execution, msize)
 {
-    auto code = from_hex("60aa6022535960005360016000f3");
-    auto r = evmone::execute(29, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    EXPECT_EQ(r.output_size, 1);
-    EXPECT_EQ(r.output_data[0], 0x40);
-    r.release(&r);
+    execute(29, "60aa6022535960005360016000f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    EXPECT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 0x40);
 }
 
-TEST(execution, gas)
+TEST_F(execution, gas)
 {
-    auto code = from_hex("5a5a5a010160005360016000f3");
-    auto r = evmone::execute(40, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 13);
-    EXPECT_EQ(r.output_size, 1);
-    EXPECT_EQ(r.output_data[0], 38 + 36 + 34);
-    r.release(&r);
+    execute(40, "5a5a5a010160005360016000f3");
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 13);
+    EXPECT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 38 + 36 + 34);
 }
 
-TEST(execution, arith)
+TEST_F(execution, arith)
 {
     // x = (0 - 1) * 3
     // y = 17 s/ x
@@ -113,16 +123,14 @@ TEST(execution, arith)
     s += "910201";                // 17 17
     s += "0315";                  // 1
     s += "60005360016000f3";
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(100, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 26);
-    EXPECT_EQ(r.output_size, 1);
-    EXPECT_EQ(r.output_data[0], 1);
-    r.release(&r);
+    execute(100, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 26);
+    EXPECT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 1);
 }
 
-TEST(execution, comparison)
+TEST_F(execution, comparison)
 {
     std::string s;
     s += "60006001808203";  // 0 1 -1
@@ -132,20 +140,18 @@ TEST(execution, comparison)
     s += "818113600353";    // m[3] = -1 s> 1
     s += "818114600453";    // m[4] = -1 == 1
     s += "60056000f3";
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(100, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 1);
-    EXPECT_EQ(r.output_size, 5);
-    EXPECT_EQ(r.output_data[0], 0);
-    EXPECT_EQ(r.output_data[1], 1);
-    EXPECT_EQ(r.output_data[2], 1);
-    EXPECT_EQ(r.output_data[3], 0);
-    EXPECT_EQ(r.output_data[4], 0);
-    r.release(&r);
+    execute(100, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 1);
+    EXPECT_EQ(result.output_size, 5);
+    EXPECT_EQ(result.output_data[0], 0);
+    EXPECT_EQ(result.output_data[1], 1);
+    EXPECT_EQ(result.output_data[2], 1);
+    EXPECT_EQ(result.output_data[3], 0);
+    EXPECT_EQ(result.output_data[4], 0);
 }
 
-TEST(execution, bitwise)
+TEST_F(execution, bitwise)
 {
     std::string s;
     s += "60aa60ff";      // aa ff
@@ -153,18 +159,16 @@ TEST(execution, bitwise)
     s += "818117600153";  // m[1] = aa | ff
     s += "818118600253";  // m[2] = aa ^ ff
     s += "60036000f3";
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(60, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 3);
-    EXPECT_EQ(r.output_data[0], 0xaa & 0xff);
-    EXPECT_EQ(r.output_data[1], 0xaa | 0xff);
-    EXPECT_EQ(r.output_data[2], 0xaa ^ 0xff);
-    r.release(&r);
+    execute(60, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 3);
+    EXPECT_EQ(result.output_data[0], 0xaa & 0xff);
+    EXPECT_EQ(result.output_data[1], 0xaa | 0xff);
+    EXPECT_EQ(result.output_data[2], 0xaa ^ 0xff);
 }
 
-TEST(execution, jump)
+TEST_F(execution, jump)
 {
     std::string s;
     s += "60be600053";  // m[0] = be
@@ -175,33 +179,29 @@ TEST(execution, jump)
     s += "5b";          // JUMPDEST
     s += "600153";      // m[1] = fa
     s += "60026000f3";  // RETURN(0,2)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(44, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 2);
-    EXPECT_EQ(r.output_data[0], 0xbe);
-    EXPECT_EQ(r.output_data[1], 0xfa);
-    r.release(&r);
+    execute(44, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 2);
+    EXPECT_EQ(result.output_data[0], 0xbe);
+    EXPECT_EQ(result.output_data[1], 0xfa);
 }
 
-TEST(execution, jumpi)
+TEST_F(execution, jumpi)
 {
     std::string s;
     s += "5a600557";      // GAS 5 JUMPI
     s += "00";            // STOP
     s += "5b60016000f3";  // JUMPDEST RETURN(0,1)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(25, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 1);
-    EXPECT_EQ(r.output_data[0], 0);
-    r.release(&r);
+    execute(25, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 0);
 }
 
 
-TEST(execution, byte)
+TEST_F(execution, byte)
 {
     std::string s;
     s += "63aabbccdd";  // aabbccdd
@@ -212,18 +212,16 @@ TEST(execution, byte)
     s += "80601f1a";    // DUP 31 BYTE
     s += "600453";      // m[4] = dd
     s += "60056000f3";  // RETURN(0,5)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(57, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 5);
-    EXPECT_EQ(r.output_data[0], 0);
-    EXPECT_EQ(r.output_data[2], 0xaa);
-    EXPECT_EQ(r.output_data[4], 0xdd);
-    r.release(&r);
+    execute(57, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 5);
+    EXPECT_EQ(result.output_data[0], 0);
+    EXPECT_EQ(result.output_data[2], 0xaa);
+    EXPECT_EQ(result.output_data[4], 0xdd);
 }
 
-TEST(execution, addmod_mulmod)
+TEST_F(execution, addmod_mulmod)
 {
     std::string s;
     s += "7fcdeb8272fc01d4d50a6ec165d2ea477af19b9b2c198459f59079583b97e88a66";
@@ -234,19 +232,17 @@ TEST(execution, addmod_mulmod)
     s += "82828209";    // DUP DUP DUP MULMOD
     s += "602052";      // m[32..]
     s += "60406000f3";  // RETURN(0,64)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(67, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 64);
+    execute(67, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 64);
     auto a = from_hex("65ef55f81fe142622955e990252cb5209a11d4db113d842408fd9c7ae2a29a5a");
-    EXPECT_EQ(a, bytes(&r.output_data[0], 32));
+    EXPECT_EQ(a, bytes(&result.output_data[0], 32));
     auto p = from_hex("34e04890131a297202753cae4c72efd508962c9129aed8b08c8e87ab425b7258");
-    EXPECT_EQ(p, bytes(&r.output_data[32], 32));
-    r.release(&r);
+    EXPECT_EQ(p, bytes(&result.output_data[32], 32));
 }
 
-TEST(execution, signextend)
+TEST_F(execution, signextend)
 {
     std::string s;
     s += "62017ffe";    // 017ffe
@@ -255,19 +251,17 @@ TEST(execution, signextend)
     s += "8060010b";    // DUP SIGNEXTEND(1)
     s += "602052";      // m[32..]
     s += "60406000f3";  // RETURN(0,64)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(49, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 64);
+    execute(49, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 64);
     auto a = from_hex("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe");
-    EXPECT_EQ(bytes(&r.output_data[0], 32), a);
+    EXPECT_EQ(bytes(&result.output_data[0], 32), a);
     auto b = from_hex("0000000000000000000000000000000000000000000000000000000000007ffe");
-    EXPECT_EQ(bytes(&r.output_data[32], 32), b);
-    r.release(&r);
+    EXPECT_EQ(bytes(&result.output_data[32], 32), b);
 }
 
-TEST(execution, exp)
+TEST_F(execution, exp)
 {
     std::string s;
     s += "612019";      // 0x2019
@@ -275,12 +269,10 @@ TEST(execution, exp)
     s += "0a";          // EXP
     s += "600052";      // m[0..]
     s += "60206000f3";  // RETURN(0,32)
-    auto code = from_hex(s.c_str());
-    auto r = evmone::execute(131, &code[0], code.size());
-    EXPECT_EQ(r.status_code, EVMC_SUCCESS);
-    EXPECT_EQ(r.gas_left, 0);
-    ASSERT_EQ(r.output_size, 32);
+    execute(131, s);
+    EXPECT_EQ(result.status_code, EVMC_SUCCESS);
+    EXPECT_EQ(result.gas_left, 0);
+    ASSERT_EQ(result.output_size, 32);
     auto a = from_hex("263cf24662b24c371a647c1340022619306e431bf3a4298d4b5998a3f1c1aaa3");
-    EXPECT_EQ(bytes(&r.output_data[0], 32), a);
-    r.release(&r);
+    EXPECT_EQ(bytes(&result.output_data[0], 32), a);
 }