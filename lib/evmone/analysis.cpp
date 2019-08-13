christianparpart/evmone

Handle small pushes more efficiently

lib/evmone/analysis.cpp
@@ -28,6 +28,14 @@ inline constexpr evmc_call_kind op2call_kind(uint8_t opcode) noexcept
     }
 }
 
+inline constexpr uint64_t load64be(const unsigned char* data) noexcept
+{
+    return uint64_t{data[7]} | (uint64_t{data[6]} << 8) | (uint64_t{data[5]} << 16) |
+           (uint64_t{data[4]} << 24) | (uint64_t{data[3]} << 32) | (uint64_t{data[2]} << 40) |
+           (uint64_t{data[1]} << 48) | (uint64_t{data[0]} << 56);
+}
+
+
 code_analysis analyze(
     const exec_fn_table& fns, evmc_revision rev, const uint8_t* code, size_t code_size) noexcept
 {
@@ -87,7 +95,21 @@ code_analysis analyze(
 
         switch (opcode)
         {
-        case ANY_PUSH:
+        case ANY_SMALL_PUSH:
+        {
+            const auto push_size = size_t(opcode - OP_PUSH1 + 1);
+            uint8_t data[8]{};
+
+            const auto leading_zeros = 8 - push_size;
+            const auto i = code_pos - code + 1;
+            for (size_t j = 0; j < push_size && (i + j) < code_size; ++j)
+                data[leading_zeros + j] = code[i + j];
+            instr.arg.number64 = load64be(data);
+            code_pos += push_size;
+            break;
+        }
+
+        case ANY_LARGE_PUSH:
         {
             // OPT: bswap data here.
             const auto push_size = size_t(opcode - OP_PUSH1 + 1);

lib/evmone/analysis.hpp
@@ -129,6 +129,7 @@ union instr_argument
         evmc_call_kind call_kind;
     } p;
     const uint8_t* data;
+    uint64_t number64;
 };
 
 static_assert(sizeof(instr_argument) == sizeof(void*), "Incorrect size of instr_argument");

lib/evmone/instructions.cpp
@@ -660,9 +660,13 @@ void op_gaslimit(execution_state& state, instr_argument) noexcept
     state.stack.push(block_gas_limit);
 }
 
+void op_push_small(execution_state& state, instr_argument arg) noexcept
+{
+    state.stack.push(arg.number64);
+}
+
 void op_push_full(execution_state& state, instr_argument arg) noexcept
 {
-    // OPT: For smaller pushes, use pointer data directly.
     state.stack.push(intx::be::uint256(arg.data));
 }
 
@@ -1249,7 +1253,9 @@ constexpr exec_fn_table create_op_table_frontier() noexcept
     table[OP_MSIZE] = op_msize;
     table[OP_GAS] = op_gas;
     table[OPX_BEGINBLOCK] = opx_beginblock;  // Replaces JUMPDEST.
-    for (auto op = size_t{OP_PUSH1}; op <= OP_PUSH32; ++op)
+    for (auto op = size_t{OP_PUSH1}; op <= OP_PUSH8; ++op)
+        table[op] = op_push_small;
+    for (auto op = size_t{OP_PUSH9}; op <= OP_PUSH32; ++op)
         table[op] = op_push_full;
     for (auto op = size_t{OP_DUP1}; op <= OP_DUP16; ++op)
         table[op] = op_dup;

luxcpp/cevm

lib/evmone/analysis.cpp
@@ -28,6 +28,14 @@ inline constexpr evmc_call_kind op2call_kind(uint8_t opcode) noexcept
     }
 }
 
+inline constexpr uint64_t load64be(const unsigned char* data) noexcept
+{
+    return uint64_t{data[7]} | (uint64_t{data[6]} << 8) | (uint64_t{data[5]} << 16) |
+           (uint64_t{data[4]} << 24) | (uint64_t{data[3]} << 32) | (uint64_t{data[2]} << 40) |
+           (uint64_t{data[1]} << 48) | (uint64_t{data[0]} << 56);
+}
+
+
 code_analysis analyze(
     const exec_fn_table& fns, evmc_revision rev, const uint8_t* code, size_t code_size) noexcept
 {
@@ -87,7 +95,21 @@ code_analysis analyze(
 
         switch (opcode)
         {
-        case ANY_PUSH:
+        case ANY_SMALL_PUSH:
+        {
+            const auto push_size = size_t(opcode - OP_PUSH1 + 1);
+            uint8_t data[8]{};
+
+            const auto leading_zeros = 8 - push_size;
+            const auto i = code_pos - code + 1;
+            for (size_t j = 0; j < push_size && (i + j) < code_size; ++j)
+                data[leading_zeros + j] = code[i + j];
+            instr.arg.small_push_value = load64be(data);
+            code_pos += push_size;
+            break;
+        }
+
+        case ANY_LARGE_PUSH:
         {
             // OPT: bswap data here.
             const auto push_size = size_t(opcode - OP_PUSH1 + 1);

lib/evmone/analysis.hpp
@@ -129,6 +129,7 @@ union instr_argument
         evmc_call_kind call_kind;
     } p;
     const uint8_t* data;
+    uint64_t small_push_value;
 };
 
 static_assert(sizeof(instr_argument) == sizeof(void*), "Incorrect size of instr_argument");

lib/evmone/instructions.cpp
@@ -660,9 +660,13 @@ void op_gaslimit(execution_state& state, instr_argument) noexcept
     state.stack.push(block_gas_limit);
 }
 
+void op_push_small(execution_state& state, instr_argument arg) noexcept
+{
+    state.stack.push(arg.small_push_value);
+}
+
 void op_push_full(execution_state& state, instr_argument arg) noexcept
 {
-    // OPT: For smaller pushes, use pointer data directly.
     state.stack.push(intx::be::uint256(arg.data));
 }
 
@@ -1249,7 +1253,9 @@ constexpr exec_fn_table create_op_table_frontier() noexcept
     table[OP_MSIZE] = op_msize;
     table[OP_GAS] = op_gas;
     table[OPX_BEGINBLOCK] = opx_beginblock;  // Replaces JUMPDEST.
-    for (auto op = size_t{OP_PUSH1}; op <= OP_PUSH32; ++op)
+    for (auto op = size_t{OP_PUSH1}; op <= OP_PUSH8; ++op)
+        table[op] = op_push_small;
+    for (auto op = size_t{OP_PUSH9}; op <= OP_PUSH32; ++op)
         table[op] = op_push_full;
     for (auto op = size_t{OP_DUP1}; op <= OP_DUP16; ++op)
         table[op] = op_dup;