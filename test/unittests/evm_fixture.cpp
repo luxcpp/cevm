ethereum/evmone

test: Helper to check if current VM is Advanced

test/unittests/evm_fixture.cpp
@@ -27,4 +27,9 @@ const char* print_vm_name(const testing::TestParamInfo<evmc::VM*>& info) noexcep
 
 INSTANTIATE_TEST_SUITE_P(
     evmone, evm, testing::Values(&advanced_vm, &baseline_vm, &bnocgoto_vm), print_vm_name);
+
+bool evm::isAdvanced() noexcept
+{
+    return GetParam() == &advanced_vm;
+}
 }  // namespace evmone::test

luxcpp/cevm

test/unittests/evm_fixture.cpp
@@ -27,4 +27,9 @@ const char* print_vm_name(const testing::TestParamInfo<evmc::VM*>& info) noexcep
 
 INSTANTIATE_TEST_SUITE_P(
     evmone, evm, testing::Values(&advanced_vm, &baseline_vm, &bnocgoto_vm), print_vm_name);
+
+bool evm::is_advanced() noexcept
+{
+    return GetParam() == &advanced_vm;
+}
 }  // namespace evmone::test