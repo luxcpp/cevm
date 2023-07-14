X-oss-byte/Evmone

state: Rename Transaction::{Kind→Type}
Use name "type" instead of "kind" to be closer to the specification.

test/state/state.cpp
@@ -57,9 +57,9 @@ int64_t compute_tx_intrinsic_cost(evmc_revision rev, const Transaction& tx) noex
 std::variant<int64_t, std::error_code> validate_transaction(const Account& sender_acc,
     const BlockInfo& block, const Transaction& tx, evmc_revision rev) noexcept
 {
-    switch (tx.kind)
+    switch (tx.type)
     {
-    case Transaction::Kind::eip1559:
+    case Transaction::Type::eip1559:
         if (rev < EVMC_LONDON)
             return make_error_code(TX_TYPE_NOT_SUPPORTED);
 
@@ -67,12 +67,12 @@ std::variant<int64_t, std::error_code> validate_transaction(const Account& sende
             return make_error_code(TIP_GT_FEE_CAP);  // Priority gas price is too high.
         [[fallthrough]];
 
-    case Transaction::Kind::eip2930:
+    case Transaction::Type::eip2930:
         if (rev < EVMC_BERLIN)
             return make_error_code(TX_TYPE_NOT_SUPPORTED);
         [[fallthrough]];
 
-    case Transaction::Kind::legacy:;
+    case Transaction::Type::legacy:;
     }
 
     assert(tx.max_priority_gas_price <= tx.max_gas_price);
@@ -204,7 +204,7 @@ std::variant<TransactionReceipt, std::error_code> transition(
     std::erase_if(state.get_accounts(),
         [](const std::pair<const address, Account>& p) noexcept { return p.second.destructed; });
 
-    auto receipt = TransactionReceipt{tx.kind, result.status_code, gas_used, host.take_logs(), {}};
+    auto receipt = TransactionReceipt{tx.type, result.status_code, gas_used, host.take_logs(), {}};
 
     // Cannot put it into constructor call because logs are std::moved from host instance.
     receipt.logs_bloom_filter = compute_bloom_filter(receipt.logs);
@@ -219,13 +219,13 @@ std::variant<TransactionReceipt, std::error_code> transition(
 
 [[nodiscard]] bytes rlp_encode(const Transaction& tx)
 {
-    if (tx.kind == Transaction::Kind::legacy)
+    if (tx.type == Transaction::Type::legacy)
     {
         // rlp [nonce, gas_price, gas_limit, to, value, data, v, r, s];
         return rlp::encode_tuple(tx.nonce, tx.max_gas_price, static_cast<uint64_t>(tx.gas_limit),
             tx.to.has_value() ? tx.to.value() : bytes_view(), tx.value, tx.data, tx.v, tx.r, tx.s);
     }
-    else if (tx.kind == Transaction::Kind::eip2930)
+    else if (tx.type == Transaction::Type::eip2930)
     {
         if (tx.v > 1)
             throw std::invalid_argument("`v` value for eip2930 transaction must be 0 or 1");
@@ -254,7 +254,7 @@ std::variant<TransactionReceipt, std::error_code> transition(
 
 [[nodiscard]] bytes rlp_encode(const TransactionReceipt& receipt)
 {
-    const auto prefix = receipt.kind == Transaction::Kind::eip1559 ? bytes{0x02} : bytes{};
+    const auto prefix = receipt.kind == Transaction::Type::eip1559 ? bytes{0x02} : bytes{};
     return prefix + rlp::encode_tuple(receipt.status == EVMC_SUCCESS,
                         static_cast<uint64_t>(receipt.gas_used),
                         bytes_view(receipt.logs_bloom_filter), receipt.logs);

test/state/state.hpp
@@ -93,7 +93,11 @@ using AccessList = std::vector<std::pair<address, std::vector<bytes32>>>;
 
 struct Transaction
 {
-    enum class Kind : uint8_t
+    /// The type of the transaction.
+    ///
+    /// The format is defined by EIP-2718: Typed Transaction Envelope.
+    /// https://eips.ethereum.org/EIPS/eip-2718.
+    enum class Type : uint8_t
     {
         /// The legacy RLP-encoded transaction without leading "type" byte.
         legacy = 0,
@@ -107,7 +111,7 @@ struct Transaction
         eip1559 = 2,
     };
 
-    Kind kind = Kind::legacy;
+    Type type = Type::legacy;
     bytes data;
     int64_t gas_limit;
     intx::uint256 max_gas_price;
@@ -132,7 +136,7 @@ struct Log
 
 struct TransactionReceipt
 {
-    Transaction::Kind kind = Transaction::Kind::legacy;
+    Transaction::Type kind = Transaction::Type::legacy;
     evmc_status_code status = EVMC_INTERNAL_ERROR;
     int64_t gas_used = 0;
     std::vector<Log> logs;

test/unittests/state_mpt_hash_test.cpp
@@ -70,7 +70,7 @@ TEST(state_mpt_hash, one_transactions)
 
     Transaction tx{};
 
-    tx.kind = Transaction::Kind::eip1559;
+    tx.type = Transaction::Type::eip1559;
     tx.data =
         "04a7e62e00000000000000000000000000000000000000000000000000000000000000c0000000000000000000"
         "000000000000000000000000000000000000000000024000000000000000000000000000000000000000000000"
@@ -186,7 +186,7 @@ TEST(state_mpt_hash, legacy_and_eip1559_receipt_three_logs_no_logs)
     //}
 
     TransactionReceipt receipt0{};
-    receipt0.kind = evmone::state::Transaction::Kind::legacy;
+    receipt0.kind = evmone::state::Transaction::Type::legacy;
     receipt0.status = EVMC_SUCCESS;
     receipt0.gas_used = 0x24522;
 
@@ -236,7 +236,7 @@ TEST(state_mpt_hash, legacy_and_eip1559_receipt_three_logs_no_logs)
     //}
 
     TransactionReceipt receipt1{};
-    receipt1.kind = evmone::state::Transaction::Kind::eip1559;
+    receipt1.kind = evmone::state::Transaction::Type::eip1559;
     receipt1.status = EVMC_SUCCESS;
     receipt1.gas_used = 0x2cd9b;
     receipt1.logs_bloom_filter = compute_bloom_filter(receipt1.logs);

luxcpp/cevm

test/state/state.cpp
@@ -57,9 +57,9 @@ int64_t compute_tx_intrinsic_cost(evmc_revision rev, const Transaction& tx) noex
 std::variant<int64_t, std::error_code> validate_transaction(const Account& sender_acc,
     const BlockInfo& block, const Transaction& tx, evmc_revision rev) noexcept
 {
-    switch (tx.kind)
+    switch (tx.type)
     {
-    case Transaction::Kind::eip1559:
+    case Transaction::Type::eip1559:
         if (rev < EVMC_LONDON)
             return make_error_code(TX_TYPE_NOT_SUPPORTED);
 
@@ -67,12 +67,12 @@ std::variant<int64_t, std::error_code> validate_transaction(const Account& sende
             return make_error_code(TIP_GT_FEE_CAP);  // Priority gas price is too high.
         [[fallthrough]];
 
-    case Transaction::Kind::eip2930:
+    case Transaction::Type::eip2930:
         if (rev < EVMC_BERLIN)
             return make_error_code(TX_TYPE_NOT_SUPPORTED);
         [[fallthrough]];
 
-    case Transaction::Kind::legacy:;
+    case Transaction::Type::legacy:;
     }
 
     assert(tx.max_priority_gas_price <= tx.max_gas_price);
@@ -205,7 +205,7 @@ std::variant<TransactionReceipt, std::error_code> transition(
     std::erase_if(state.get_accounts(),
         [](const std::pair<const address, Account>& p) noexcept { return p.second.destructed; });
 
-    auto receipt = TransactionReceipt{tx.kind, result.status_code, gas_used, host.take_logs(), {}};
+    auto receipt = TransactionReceipt{tx.type, result.status_code, gas_used, host.take_logs(), {}};
 
     // Cannot put it into constructor call because logs are std::moved from host instance.
     receipt.logs_bloom_filter = compute_bloom_filter(receipt.logs);
@@ -220,13 +220,13 @@ std::variant<TransactionReceipt, std::error_code> transition(
 
 [[nodiscard]] bytes rlp_encode(const Transaction& tx)
 {
-    if (tx.kind == Transaction::Kind::legacy)
+    if (tx.type == Transaction::Type::legacy)
     {
         // rlp [nonce, gas_price, gas_limit, to, value, data, v, r, s];
         return rlp::encode_tuple(tx.nonce, tx.max_gas_price, static_cast<uint64_t>(tx.gas_limit),
             tx.to.has_value() ? tx.to.value() : bytes_view(), tx.value, tx.data, tx.v, tx.r, tx.s);
     }
-    else if (tx.kind == Transaction::Kind::eip2930)
+    else if (tx.type == Transaction::Type::eip2930)
     {
         if (tx.v > 1)
             throw std::invalid_argument("`v` value for eip2930 transaction must be 0 or 1");
@@ -255,7 +255,7 @@ std::variant<TransactionReceipt, std::error_code> transition(
 
 [[nodiscard]] bytes rlp_encode(const TransactionReceipt& receipt)
 {
-    const auto prefix = receipt.kind == Transaction::Kind::eip1559 ? bytes{0x02} : bytes{};
+    const auto prefix = receipt.type == Transaction::Type::eip1559 ? bytes{0x02} : bytes{};
     return prefix + rlp::encode_tuple(receipt.status == EVMC_SUCCESS,
                         static_cast<uint64_t>(receipt.gas_used),
                         bytes_view(receipt.logs_bloom_filter), receipt.logs);

test/state/state.hpp
@@ -93,7 +93,11 @@ using AccessList = std::vector<std::pair<address, std::vector<bytes32>>>;
 
 struct Transaction
 {
-    enum class Kind : uint8_t
+    /// The type of the transaction.
+    ///
+    /// The format is defined by EIP-2718: Typed Transaction Envelope.
+    /// https://eips.ethereum.org/EIPS/eip-2718.
+    enum class Type : uint8_t
     {
         /// The legacy RLP-encoded transaction without leading "type" byte.
         legacy = 0,
@@ -107,7 +111,7 @@ struct Transaction
         eip1559 = 2,
     };
 
-    Kind kind = Kind::legacy;
+    Type type = Type::legacy;
     bytes data;
     int64_t gas_limit;
     intx::uint256 max_gas_price;
@@ -132,7 +136,7 @@ struct Log
 
 struct TransactionReceipt
 {
-    Transaction::Kind kind = Transaction::Kind::legacy;
+    Transaction::Type type = Transaction::Type::legacy;
     evmc_status_code status = EVMC_INTERNAL_ERROR;
     int64_t gas_used = 0;
     std::vector<Log> logs;

test/unittests/state_mpt_hash_test.cpp
@@ -70,7 +70,7 @@ TEST(state_mpt_hash, one_transactions)
 
     Transaction tx{};
 
-    tx.kind = Transaction::Kind::eip1559;
+    tx.type = Transaction::Type::eip1559;
     tx.data =
         "04a7e62e00000000000000000000000000000000000000000000000000000000000000c0000000000000000000"
         "000000000000000000000000000000000000000000024000000000000000000000000000000000000000000000"
@@ -186,7 +186,7 @@ TEST(state_mpt_hash, legacy_and_eip1559_receipt_three_logs_no_logs)
     //}
 
     TransactionReceipt receipt0{};
-    receipt0.kind = evmone::state::Transaction::Kind::legacy;
+    receipt0.type = evmone::state::Transaction::Type::legacy;
     receipt0.status = EVMC_SUCCESS;
     receipt0.gas_used = 0x24522;
 
@@ -236,7 +236,7 @@ TEST(state_mpt_hash, legacy_and_eip1559_receipt_three_logs_no_logs)
     //}
 
     TransactionReceipt receipt1{};
-    receipt1.kind = evmone::state::Transaction::Kind::eip1559;
+    receipt1.type = evmone::state::Transaction::Type::eip1559;
     receipt1.status = EVMC_SUCCESS;
     receipt1.gas_used = 0x2cd9b;
     receipt1.logs_bloom_filter = compute_bloom_filter(receipt1.logs);