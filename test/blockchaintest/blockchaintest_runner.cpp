megaeth-labs/evmone-compiler

blockchaintest: Execute system call

test/blockchaintest/blockchaintest_runner.cpp
@@ -30,6 +30,8 @@ static TransitionResult apply_block(state::State& state, evmc::VM& vm,
     const state::BlockInfo& block, const std::vector<state::Transaction>& txs, evmc_revision rev,
     std::optional<int64_t> block_reward)
 {
+    state::system_calls(state, block, rev, vm);
+
     std::vector<state::Log> txs_logs;
     int64_t block_gas_left = block.gas_limit;
 

luxcpp/cevm

test/blockchaintest/blockchaintest_runner.cpp
@@ -33,6 +33,8 @@ TransitionResult apply_block(state::State& state, evmc::VM& vm, const state::Blo
     const std::vector<state::Transaction>& txs, evmc_revision rev,
     std::optional<int64_t> block_reward)
 {
+    state::system_call(state, block, rev, vm);
+
     std::vector<state::Log> txs_logs;
     int64_t block_gas_left = block.gas_limit;
 